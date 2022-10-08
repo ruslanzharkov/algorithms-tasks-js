7ad1492d9130401fb9597ad09d0062355f1ebbdc
Ruslan Zharkov <zharkov.ruslan.e@gmail.com>
2022-10-08 21:59:39 +0300

    Create curry.js

---
+++ curry.js
@@ -0,0 +1,11 @@
+function curry(fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    } else {
+      return function(...nextArgs) {
+        return curried.apply(null, args.concat(nextArgs));
+      }
+    }
+  }
+}
