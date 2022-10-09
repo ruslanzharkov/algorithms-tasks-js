66564d82f044960e1f792dfc9d585e9d18e76c99
Ruslan Zharkov <zharkov.ruslan.e@gmail.com>
2022-10-09 21:05:55 +0300

    Create composition.js

---
+++ challenges/composition.js
@@ -0,0 +1,14 @@
+
+
+/**
+ * @param {Array<(arg: any) => any>} funcs 
+ * @return {(arg: any) => any}
+ */
+function pipe(funcs) {
+  return function(value) {
+		const res = funcs.reduce((acc, val) => {
+			return val(acc)
+		}, value);
+		return res;
+	}
+}
