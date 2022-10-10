cb613c362e9a7d0337c0a83a1f438972b5c96c1d
Ruslan Zharkov <zharkov.ruslan.e@gmail.com>
2022-10-10 22:35:08 +0300

    Update fibonacci.js

--- challenges/fibonacci.js
+++ challenges/fibonacci.js
@@ -3,7 +3,7 @@ function fib(n, memo = {}) {
   if(n == 0 || n == 1) return n;
 
   if(memo[n]) {
-      return memo[n];
+    return memo[n];
   }
   memo[n] = fib(n - 2, memo) + fib(n - 1, memo);
   
@@ -12,14 +12,14 @@ function fib(n, memo = {}) {
 
 // iterative
 function fib(n) {
-    if(n <= 1) return n;
-    
-    var pre1 = 0, pre2 = 1;
-    
-    for(var i = 2; i<=n; i++) {
-        var cur = pre1 + pre2;
-        pre1 = pre2;
-        pre2 = cur;
-    }
-    return pre2;
+  if(n <= 1) return n;
+
+  var pre1 = 0, pre2 = 1;
+
+  for(var i = 2; i<=n; i++) {
+    var cur = pre1 + pre2;
+    pre1 = pre2;
+    pre2 = cur;
+  }
+  return pre2;
 }
