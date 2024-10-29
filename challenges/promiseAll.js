a73f305c29ea8173c1d9f6f507eafaa6ebcac12a
Ruslan Zharkov <zharkov.ruslan.e@gmail.com>
2024-10-29 19:36:31 +0100

    Create promiseAll.js

---
+++ challenges/promiseAll.js
@@ -0,0 +1,31 @@
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = function (functions) {
+    return new Promise((res, rej) => {
+        const arr = new Array(functions.length)
+        let resolvedCount = 0
+
+        functions.forEach((func, index) => {
+            func()
+                .then(val => {
+                    arr[index] = val
+                    resolvedCount++
+
+                    if (resolvedCount === functions.length) {
+                        res(arr)
+                    }
+
+                })
+                .catch(e => {
+                    rej(e)
+                })
+        })
+    })
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
